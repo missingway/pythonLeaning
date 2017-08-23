51e1e98df4fd9d527987c028581fc860e524ef6f
missingway <missingway2000@126.com>
2017-08-23 20:55:54 +0800

    add exercise folder

---
+++ exercise/1.py
@@ -0,0 +1,31 @@
+#print('Twinkle, twinkle, little star,\n\t\tHow I wonder what you are! \n\t\t\t\tUp above the world so high, \n\t\t\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\t\tHow I wonder what you are')
+
+"""import platform
+print(platform.python_version())
+import sys
+print(sys.version)"""
+
+"""import time
+t=time.time()
+i=time.localtime(t)
+m=time.strftime('%Y-%m-%d %H:%M:%S',i)
+print("day and time:\n%s" % m)"""
+
+"""import math
+def ci(radius):
+	i=float(radius)
+	area=i*i*math.pi
+	return area
+
+c=input("please tell me a circle radius")
+print(str(ci(c)))"""
+
+### question 5
+"""def name(f,l):
+	n=l+' '+f
+	return n
+
+firstname=input("please tell me your firstname")
+lastname=input("please tell me your lastname")
+n=name(firstname,lastname)
+print(n)"""
